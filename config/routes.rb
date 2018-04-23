cd087646af0329b0d351e22ac556c092d3e60c28
Path17 <adnebril@up.edu.ph>
2018-04-24 01:10:37 +0800

    Added necessary controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
   get '/finished_jobs' => 'job_orders#finished_jobs'
   get '/trash' => 'job_orders#trash'
   post '/pending_requests' => 'job_orders#pending_requests'
+
+  resources :users, only: [:new, :create]
+  resources :user_sessions, only: [:create, :destroy]
+  delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
+  get '/sign_in', to: 'user_sessions#new', as: :sign_in
 end
