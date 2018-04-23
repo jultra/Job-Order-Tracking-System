956d0d38efa344940fd972e191095fcf4cf97e19
Path17 <adnebril@up.edu.ph>
2018-04-24 01:00:29 +0800

    Setup AuthLogic Authentication System Phase 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409072250) do
+ActiveRecord::Schema.define(version: 20180423165332) do
 
   create_table "job_orders", force: :cascade do |t|
     t.string "control_no"
@@ -38,16 +38,39 @@ ActiveRecord::Schema.define(version: 20180409072250) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
-    t.string "password"
     t.string "position"
     t.string "fname"
     t.string "mname"
     t.string "lname"
+    t.string "email"
+    t.string "crypted_password"
+    t.string "password_salt"
+    t.string "persistence_token"
+    t.string "single_access_token"
+    t.string "perishable_token"
+    t.integer "login_count", default: 0, null: false
+    t.integer "failed_login_count", default: 0, null: false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string "current_login_ip"
+    t.string "last_login_ip"
+    t.boolean "active", default: false
+    t.boolean "approved", default: false
+    t.boolean "confirmed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["perishable_token"], name: "index_users_on_perishable_token", unique: true
+    t.index ["persistence_token"], name: "index_users_on_persistence_token", unique: true
+    t.index ["single_access_token"], name: "index_users_on_single_access_token", unique: true
   end
 
 end
