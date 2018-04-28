b8615e4f033097793c025c75cfa47b8cb3f3b5c0
jhamezzz1315 <jkkuizon@up.edu.ph>
2018-04-28 18:54:25 +0800

    Validates done

--- app/controllers/job_orders_controller.rb
+++ app/controllers/job_orders_controller.rb
@@ -2,39 +2,29 @@ class JobOrdersController < ApplicationController
   protect_from_forgery
 
   def request_form
-    control_no = params['control_no']
-    where = params['where']
-    date_needed = params['date_needed']
-    time_needed = params['time_needed']
-    problem_area = params['problem_area']
-    requester = params['requester']
-    adviser = params['adviser']
-    fund_source = params['fund_source']
+    new_request = JobOrder.new
+    new_request.control_no = params['control_no']
+    new_request.where = params['where']
+    new_request.date_needed = params['date_needed']
+    new_request.time_needed = params['time_needed']
+    new_request.information = params['problem_area']
+    new_request.requester = params['requester']
+    new_request.fund_source = params['fund_source']
+    new_request.adviser = params['adviser']
 
-    if(control_no != nil && where != nil && date_needed != nil && time_needed != nil && problem_area != nil )
-      new_request = JobOrder.new
-      new_request.control_no = control_no
-      new_request.where = where
-      new_request.date_needed = date_needed
-      new_request.time_needed = time_needed
-      new_request.information = problem_area
-      new_request.requester = requester
-      new_request.fund_source = fund_source
-      new_request.adviser = adviser
-
-      for i in 1..22
-        if params["checkbox#{i}"] != nil
-          new_request.job_type = params["checkbox#{i}"];
-          end
+    for i in 1..22
+      if params["checkbox#{i}"] != nil
+        new_request.job_type = params["checkbox#{i}"];
       end
-      test_ = new_request.save
     end
 
-      if(params['submit'])
+    if (params['submit']) 
+      if new_request.save
         redirect_to '/pending_requests'
-      elsif(params['cancel'])
-        redirect_to '/index'
       end
+    elsif (params['cancel'])
+      redirect_to '/index'
+    end
   end
 
   def pending_requests
--- app/models/job_order.rb
+++ app/models/job_order.rb
@@ -1,2 +1,4 @@
 class JobOrder < ApplicationRecord
+	validates :control_no, :where, :date_needed, :time_needed, :information, :requester, :adviser, :fund_source, :job_type, presence: true
+
 end
