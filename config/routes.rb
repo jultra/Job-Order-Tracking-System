eb85c1fb97d75e340c9b0a6a499e7b6a12661b8c
Bea Santiago <BeaSantiago@BeaSantiago.localdomain>
2018-05-01 23:28:07 +0800

    refactor for job_order controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,7 @@ Rails.application.routes.draw do
   get '/login' => 'users#login'
   get '/index' => 'users#index'
   get '/signup' => 'users#sign_up'
-  get '/request_form' => 'job_orders#request_form'
-  post '/request_form' => 'job_orders#request_form'
-  get '/pending_requests' => 'job_orders#pending_requests'
-  get '/ongoing_jobs' => 'job_orders#ongoing_jobs'
-  get '/finished_jobs' => 'job_orders#finished_jobs'
-  get '/trash' => 'job_orders#trash'
-  post '/pending_requests' => 'job_orders#pending_requests'
+  get '/job_orders/list_pending_requests' => 'job_orders#list_pending_requests'
+  resources :job_orders
+  resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409072250) do
+ActiveRecord::Schema.define(version: 20180428154337) do
 
   create_table "job_orders", force: :cascade do |t|
     t.string "control_no"
@@ -36,12 +36,13 @@ ActiveRecord::Schema.define(version: 20180409072250) do
     t.date "date_started"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "progress"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "position"
     t.string "fname"
     t.string "mname"
