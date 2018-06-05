3301c5cedaef4b4db55f5cc1a20bb55954a75577
Path17 <adnebril@up.edu.ph>
2018-06-06 03:49:33 +0800

    gg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,17 @@ class UsersController < ApplicationController
   layout :resolve_layout
 
   def new
+    if current_user_session != nil
+      current_user_session.destroy
+    else
+      session.destroy
+    end
     @user = User.new
   end
 
   def create
     @user = User.new(users_params)
-    if @user.save
+    if @user.save_without_session_maintenance
       flash.keep
       flash[:success] = "Account Request Submitted"
       redirect_to root_path
