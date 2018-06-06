39e07ab94e8740c04b7e88c7572160c27fd1047f
Unknown <santiagobea79@yahoo.com.ph>
2018-06-06 14:18:53 +0800

    approvals

--- app/models/job_order.rb
+++ app/models/job_order.rb
@@ -1,4 +1,4 @@
 class JobOrder < ApplicationRecord
   validates :control_no, :where, :date_needed, :time_needed, :information, :requester, :adviser, :fund_source, :job_type, presence: true
-
+  #validates :job_office, :delivery_date, presence: true
 end
