661f34de15c7217703c5252e832bf3181cbfd09d
James Michael K. Kuizon <jameskuizon13@gmail.com>
2019-02-11 23:36:07 +0800

    Changed sqlitedb to mysql

--- Gemfile
+++ Gemfile
@@ -15,9 +15,10 @@ gem 'haml-rails'
 gem 'haml'
 gem 'jquery-rails'
 
+gem 'yaml_db'
+
 gem 'rolify'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'mysql2', '>= 0.4.4', '< 0.6.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180616102557) do
+ActiveRecord::Schema.define(version: 20180621114744) do
+
   create_table "job_orders", force: :cascade do |t|
     t.string "control_no"
     t.string "job_type"
@@ -101,7 +102,7 @@ ActiveRecord::Schema.define(version: 20180616102557) do
     t.index ["single_access_token"], name: "index_users_on_single_access_token", unique: true
   end
 
-  create_table 'users_roles', id: false, force: :cascade do |t|
+  create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "role_id"
     t.index ["role_id"], name: "index_users_roles_on_role_id"
